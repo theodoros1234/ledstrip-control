e2fc12de0877e192a154e2576789a9f16aeaf8cb
Theodoros Nicolaou <theonicolaoucostas@gmail.com>
2022-02-28 00:43:42 +0200

    Added functionality to go through different color modes by touching with a finger one of the two pins on the Arduino board that are set as prev/next controls

--- LED_strip_control/LED_strip_control.ino
+++ LED_strip_control/LED_strip_control.ino
@@ -2,9 +2,152 @@
 #define green 10
 #define blue  6
 #define cycle_rate 1536
+#define cap_pin_next 15
+#define cap_pin_prev 4
+#define next_touch_timeout 200
 bool invert=false;
-byte r_v=255,g_v=255,b_v=255,br=255,read_val,str_pos=255;
+byte r_v=255,g_v=255,b_v=255,br=255,read_val,str_pos=255,mode_sel;
 char mode='S';
+unsigned long next_touch;
+
+void change_mode(bool next) {
+  next_touch = millis()+next_touch_timeout;
+  if (next)
+    mode_sel++;
+  else
+    mode_sel--;
+  if (mode_sel==21)
+    mode_sel=0;
+  if (mode_sel==-1)
+    mode_sel=20;
+  
+  switch (mode_sel) {
+    case 0:
+      mode='S';r_v=0xff;g_v=0x1f;b_v=0x00;
+      break;
+    case 1:
+      mode='S';r_v=0xff;g_v=0x2f;b_v=0x00;
+      break;
+    case 2:
+      mode='S';r_v=0xff;g_v=0x4f;b_v=0;
+      break;
+    case 3:
+      mode='S';r_v=0xff;g_v=0x7f;b_v=0;
+      break;
+    case 4:
+      mode='S';r_v=0xff;g_v=0xff;b_v=0;
+      break;
+    case 5:
+      mode='S';r_v=0;g_v=0xff;b_v=0;
+      break;
+    case 6:
+      mode='S';r_v=0;g_v=0xff;b_v=0x7f;
+      break;
+    case 7:
+      mode='S';r_v=0;g_v=0xff;b_v=0xff;
+      break;
+    case 8:
+      mode='S';r_v=0;g_v=0x7f;b_v=0xff;
+      break;
+    case 9:
+      mode='S';r_v=0;g_v=0;b_v=0xff;
+      break;
+    case 10:
+      mode='S';r_v=0x3f;g_v=0;b_v=0xff;
+      break;
+    case 11:
+      mode='S';r_v=0xff;g_v=0;b_v=0xff;
+      break;
+    case 12:
+      mode='S';r_v=0xff;g_v=0;b_v=0x7f;
+      break;
+    case 13:
+      mode='S';r_v=0xff;g_v=0;b_v=0x3f;
+      break;
+    case 14:
+      mode='S';r_v=0xff;g_v=0xff;b_v=0xff;
+      break;
+    case 15:
+      mode='S';r_v=0xff;g_v=0xaf;b_v=0x7f;
+      break;
+    case 16:
+      mode='S';r_v=0xff;g_v=0x7f;b_v=0x3f;
+      break;
+    case 17:
+      mode='S';r_v=0;g_v=0;b_v=0;
+      break;
+    case 18:
+      mode='S';r_v=0xff;g_v=0x4f;b_v=13;
+      break;
+    case 19:
+      mode='S';r_v=0xff;g_v=0x38;b_v=0x03;
+      break;
+    case 20:
+      mode='H';br=0xff;
+      break;
+  }
+  apply_values();
+}
+
+uint8_t readCapacitivePin(int pinToMeasure) {
+  // Variables used to translate from Arduino to AVR pin naming
+  volatile uint8_t* port;
+  volatile uint8_t* ddr;
+  volatile uint8_t* pin;
+  // Here we translate the input pin number from
+  //  Arduino pin number to the AVR PORT, PIN, DDR,
+  //  and which bit of those registers we care about.
+  byte bitmask;
+  port = portOutputRegister(digitalPinToPort(pinToMeasure));
+  ddr = portModeRegister(digitalPinToPort(pinToMeasure));
+  bitmask = digitalPinToBitMask(pinToMeasure);
+  pin = portInputRegister(digitalPinToPort(pinToMeasure));
+  // Discharge the pin first by setting it low and output
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+  delay(1);
+  uint8_t SREG_old = SREG; //back up the AVR Status Register
+  // Prevent the timer IRQ from disturbing our measurement
+  noInterrupts();
+  // Make the pin an input with the internal pull-up on
+  *ddr &= ~(bitmask);
+  *port |= bitmask;
+
+  // Now see how long the pin to get pulled up. This manual unrolling of the loop
+  // decreases the number of hardware cycles between each read of the pin,
+  // thus increasing sensitivity.
+  uint8_t cycles = 17;
+  if (*pin & bitmask) { cycles =  0;}
+  else if (*pin & bitmask) { cycles =  1;}
+  else if (*pin & bitmask) { cycles =  2;}
+  else if (*pin & bitmask) { cycles =  3;}
+  else if (*pin & bitmask) { cycles =  4;}
+  else if (*pin & bitmask) { cycles =  5;}
+  else if (*pin & bitmask) { cycles =  6;}
+  else if (*pin & bitmask) { cycles =  7;}
+  else if (*pin & bitmask) { cycles =  8;}
+  else if (*pin & bitmask) { cycles =  9;}
+  else if (*pin & bitmask) { cycles = 10;}
+  else if (*pin & bitmask) { cycles = 11;}
+  else if (*pin & bitmask) { cycles = 12;}
+  else if (*pin & bitmask) { cycles = 13;}
+  else if (*pin & bitmask) { cycles = 14;}
+  else if (*pin & bitmask) { cycles = 15;}
+  else if (*pin & bitmask) { cycles = 16;}
+
+  // End of timing-critical section; turn interrupts back on if they were on before, or leave them off if they were off before
+  SREG = SREG_old;
+
+  // Discharge the pin again by setting it low and output
+  //  It's important to leave the pins low if you want to 
+  //  be able to touch more than 1 sensor at a time - if
+  //  the sensor is left pulled high, when you touch
+  //  two sensors, your body will transfer the charge between
+  //  sensors.
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+  return cycles;
+}
 
 byte hex(byte in) {
   if ('0'<=in&&in<='9')
@@ -168,5 +311,11 @@ void loop() {
       g_v=((int)g_v*br)/255;
       b_v=((int)b_v*br)/255;
       apply_values();
+  }
+  if (next_touch<millis()) {
+    if (readCapacitivePin(cap_pin_next)>1)
+      change_mode(true);
+    else if (readCapacitivePin(cap_pin_prev)>1)
+      change_mode(false);
   }
 }
